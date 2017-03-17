a28165d5c3d945211ffa570c0c36ccc8bba60806
miya0001 <miya@wpist.me>
2017-03-17 14:05:58 +0900

    remove photo

--- src/app.js
+++ src/app.js
@@ -35,11 +35,5 @@ riot.mount( map, {
       "attribution": "OpenStreetMap Contributers",
       "attribution_url": "http://osm.org/copyright"
     },
-    {
-      "name": "空中写真",
-      "tile": "https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg",
-      "attribution": "GSI Japan",
-      "attribution_url": "http://maps.gsi.go.jp/development/ichiran.html"
-    },
   ]
 } )
